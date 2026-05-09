197b7f7eea35df55645fbb51c0a6368827db968e
Елизавета Назарова <arelro88@gmail.com>
2026-05-09 20:12:23 +0300

    Delete praktika_types_unicode directory

--- praktika_types_unicode/Practice3.java
+++
@@ -1,9 +0,0 @@
-public class Practice3 {
-    public static void main(String[] args) {
-        // Правильнее дать объяснение, потому что исходный код не компилируется
-        System.out.println("Код не скомпилируется.");
-        System.out.println("Переменная b объявлена внутри блока {}.");
-        System.out.println("Вне этого блока переменная b не существует.");
-        System.out.println("Поэтому вычислить c нельзя.");
-    }
-}
--- praktika_types_unicode/praktika_types_unicode/Practice2.java
+++
@@ -1,6 +0,0 @@
-public class Practice2 {
-    public static void main(String[] args) {
-        // Имя напечатано через Unicode-коды
-        System.out.println("\u041E\u043B\u0435\u0433");
-    }
-}
