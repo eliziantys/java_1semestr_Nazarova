e27c79dce77e84f8632a1b8f94871cceb433b02b
Елизавета Назарова <arelro88@gmail.com>
2026-05-09 20:05:55 +0300

    Базовые типы данных и литералы

---
+++ praktika_types_unicode/praktika_types_unicode/Practice4.java
@@ -0,0 +1,16 @@
+public class Practice4 {
+    public static void main(String[] args) {
+        Integer year = 2026;
+        double mark = 4.5;
+
+        String text1 = "Год: " + year;
+        String text2 = "Оценка: " + mark;
+        String text3 = "Сумма: " + 5 + 7;
+        String text4 = "Сумма: " + (5 + 7);
+
+        System.out.println(text1);
+        System.out.println(text2);
+        System.out.println(text3);
+        System.out.println(text4);
+    }
+}
